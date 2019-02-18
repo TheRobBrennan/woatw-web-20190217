1b74d19563b29796417f254b01adb01fc3ab0ce3
Rob Brennan <rob@therobbrennan.com>
2019-02-17 21:05:41 -0800

    Moved Header into its own folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
 
-import Header from '../components/Header'
+import Header from '../components/Header/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer/Footer'
 
