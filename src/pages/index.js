05ef5bcfe313e62285e322ff9c90244fc5fc4421
Rob Brennan <rob@therobbrennan.com>
2019-02-17 21:06:22 -0800

    Moved Main component into its own folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/Layout/Layout'
 
 import Header from '../components/Header/Header'
-import Main from '../components/Main'
+import Main from '../components/Main/Main'
 import Footer from '../components/Footer/Footer'
 
 class IndexPage extends React.Component {
