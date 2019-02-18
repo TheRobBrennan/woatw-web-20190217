0b906630b652283d71e9a3458252932431038104
Rob Brennan <rob@therobbrennan.com>
2019-02-17 20:03:52 -0800

    Refactored renderLayout into a lib folder

---
+++ src/components/Layout/lib/renderLayout.js
@@ -0,0 +1,16 @@
+import Helmet from 'react-helmet'
+
+export const renderLayout = (data, content) => (
+  <>
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        { name: 'description', content: '' },
+        { name: 'keywords', content: '' },
+      ]}
+    >
+      <html lang="en" />
+    </Helmet>
+    {content}
+  </>
+)
