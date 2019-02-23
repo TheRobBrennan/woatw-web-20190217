74c823d61b44dfc06a5f07fbc30d14dd1f1b8863
Rob Brennan <rob@therobbrennan.com>
2019-02-22 22:01:46 -0800

    Update SiteTitleQuery to retrieve Twitter meta data

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -17,6 +17,11 @@ const Layout = ({ children, location }) => {
           description
           type
           locale
+          twitter {
+            card
+            site
+            creator
+          }
         }
       }
     }
