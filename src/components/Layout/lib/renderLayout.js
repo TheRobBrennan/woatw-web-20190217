ecd90740661526f16686ea8df4da312101e59048
Rob Brennan <rob@therobbrennan.com>
2019-02-22 20:47:05 -0800

    Added open graph meta tags

--- src/components/Layout/lib/renderLayout.js
+++ src/components/Layout/lib/renderLayout.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
+// Images
+import posterForUpcomingShow from '../../../images/woatw/20190223-wyatt-olney-and-the-wreckage-tony-vs-everett-wa.jpg'
+
 export const renderLayout = (data, content) => (
   <>
     <Helmet
@@ -8,6 +11,8 @@ export const renderLayout = (data, content) => (
       meta={[
         { name: 'description', content: '' },
         { name: 'keywords', content: '' },
+        { name: 'og:description', content: data.site.siteMetadata.og.description},
+        { name: 'og:image', content: posterForUpcomingShow},
       ]}
     >
       <html lang="en" />
