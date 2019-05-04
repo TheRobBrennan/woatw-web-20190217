f9aa2ae0ede001c67ea942b1e8c931d0894dfd63
Rob Brennan <rob@therobbrennan.com>
2019-05-04 13:43:11 -0700

    Fixed broken navigation with newly corrected href

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,8 +13,8 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="/" onClick={() => {props.onOpenArticle('welcome-to-seattle')}}>Video</a></li>
-                <li><a href="/" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="/#" onClick={(e) => {e.stopPropagation(); props.onOpenArticle('welcome-to-seattle')}}>Video</a></li>
+                <li><a href="/#" onClick={(e) => {e.stopPropagation(); props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
     </header>
