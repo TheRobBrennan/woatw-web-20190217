21f73082775c221096f07ee077a485685a503592
Rob Brennan <rob@therobbrennan.com>
2019-09-11 20:12:44 -0700

    Added Halloween show to Upcoming Shows

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 // Images
 import bandPhoto from '../../images/woatw/20190430-jared-ream-woatw-photo.jpg'
 import posterForUpcomingShow from '../../images/woatw/20191012-migs-fest-iii-club-sur-seattle-wa.jpg'
+import posterForUpcomingShowAlt from '../../images/woatw/20191025-halloween-darrells-tavern-seattle-wa.jpg'
 
 // Components
 import Subscribe from '../Subscribe'
@@ -136,6 +137,16 @@ class Main extends React.Component {
               />
             </a>
           </span>
+          <hr />
+          <span className='image main'>
+            <a href="https://www.facebook.com/events/681505865659934/" target="__blank">
+              <img
+                src={posterForUpcomingShowAlt}
+                alt="2019.10.25 5th Annual Wreckage Halloween Bash - Darrell's Tavern - Seattle WA"
+              />
+            </a>
+          </span>
+
           {close}
         </article>
 
